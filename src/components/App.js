fe0ddb081db35a15ad5c9cea306b369ccfed4e40
Vellyus <noisyboy026@gmail.com>
2022-10-09 22:31:13 +0200

    test

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => (
       <Header />
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/course_directory" element={ <Home /> } />
         <Route path="/about" element={ <About title="About" /> } />
         <Route path="/teachers" element={ <Teachers /> } />
         <Route path="/teachers/:topic/:name" element={ <Featured /> } />
